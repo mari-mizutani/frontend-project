266b692628a68fd49d85078a69948927bf05c2a7
mari-mizutani <82095084+mari-mizutani@users.noreply.github.com>
2021-07-22 11:41:55 +0200

    connect api and check it in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GetApiService } from './get-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  constructor( private api:GetApiService )
+  {
+
+  }
+  ngOnInit() //call the fetch function
+  {
+    this.api.apiCall().subscribe((data)=>{
+      console.log("get api data",data);
+    })
+  }
 }
